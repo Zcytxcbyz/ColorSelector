b1b2bc8d834d3755dd0f508b46e746108442c726
zcytxcbyz@163.com <zcytxcbyz@163.com>
2019-10-05 12:09:41 +0800

    First Commit.

---
+++ ColorSelector/Form_Main.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ColorSelector
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_color = new System.Windows.Forms.Button();
+            this.panel_color = new System.Windows.Forms.Panel();
+            this.textBox_red = new System.Windows.Forms.TextBox();
+            this.textBox_green = new System.Windows.Forms.TextBox();
+            this.textBox_blue = new System.Windows.Forms.TextBox();
+            this.label_red = new System.Windows.Forms.Label();
+            this.label_green = new System.Windows.Forms.Label();
+            this.label_blue = new System.Windows.Forms.Label();
+            this.textBox_rgb = new System.Windows.Forms.TextBox();
+            this.label_rgb = new System.Windows.Forms.Label();
+            this.label_info = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_color
+            // 
+            this.button_color.Location = new System.Drawing.Point(12, 88);
+            this.button_color.Name = "button_color";
+            this.button_color.Size = new System.Drawing.Size(70, 41);
+            this.button_color.TabIndex = 0;
+            this.button_color.Text = "Select Color";
+            this.button_color.UseVisualStyleBackColor = true;
+            this.button_color.Click += new System.EventHandler(this.button_color_Click);
+            // 
+            // panel_color
+            // 
+            this.panel_color.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_color.Location = new System.Drawing.Point(12, 12);
+            this.panel_color.Name = "panel_color";
+            this.panel_color.Size = new System.Drawing.Size(70, 70);
+            this.panel_color.TabIndex = 2;
+            // 
+            // textBox_red
+            // 
+            this.textBox_red.Location = new System.Drawing.Point(131, 24);
+            this.textBox_red.Name = "textBox_red";
+            this.textBox_red.Size = new System.Drawing.Size(48, 21);
+            this.textBox_red.TabIndex = 3;
+            this.textBox_red.TextChanged += new System.EventHandler(this.textBox_red_TextChanged);
+            // 
+            // textBox_green
+            // 
+            this.textBox_green.Location = new System.Drawing.Point(131, 51);
+            this.textBox_green.Name = "textBox_green";
+            this.textBox_green.Size = new System.Drawing.Size(48, 21);
+            this.textBox_green.TabIndex = 4;
+            this.textBox_green.TextChanged += new System.EventHandler(this.textBox_green_TextChanged);
+            // 
+            // textBox_blue
+            // 
+            this.textBox_blue.Location = new System.Drawing.Point(131, 78);
+            this.textBox_blue.Name = "textBox_blue";
+            this.textBox_blue.Size = new System.Drawing.Size(48, 21);
+            this.textBox_blue.TabIndex = 5;
+            this.textBox_blue.TextChanged += new System.EventHandler(this.textBox_blue_TextChanged);
+            // 
+            // label_red
+            // 
+            this.label_red.AutoSize = true;
+            this.label_red.Location = new System.Drawing.Point(102, 27);
+            this.label_red.Name = "label_red";
+            this.label_red.Size = new System.Drawing.Size(23, 12);
+            this.label_red.TabIndex = 7;
+            this.label_red.Text = "Red";
+            // 
+            // label_green
+            // 
+            this.label_green.AutoSize = true;
+            this.label_green.Location = new System.Drawing.Point(90, 54);
+            this.label_green.Name = "label_green";
+            this.label_green.Size = new System.Drawing.Size(35, 12);
+            this.label_green.TabIndex = 8;
+            this.label_green.Text = "Green";
+            // 
+            // label_blue
+            // 
+            this.label_blue.AutoSize = true;
+            this.label_blue.Location = new System.Drawing.Point(96, 81);
+            this.label_blue.Name = "label_blue";
+            this.label_blue.Size = new System.Drawing.Size(29, 12);
+            this.label_blue.TabIndex = 9;
+            this.label_blue.Text = "Blue";
+            // 
+            // textBox_rgb
+            // 
+            this.textBox_rgb.Location = new System.Drawing.Point(41, 138);
+            this.textBox_rgb.Name = "textBox_rgb";
+            this.textBox_rgb.Size = new System.Drawing.Size(138, 21);
+            this.textBox_rgb.TabIndex = 11;
+            this.textBox_rgb.TextChanged += new System.EventHandler(this.textBox_rgb_TextChanged);
+            // 
+            // label_rgb
+            // 
+            this.label_rgb.AutoSize = true;
+            this.label_rgb.Location = new System.Drawing.Point(12, 141);
+            this.label_rgb.Name = "label_rgb";
+            this.label_rgb.Size = new System.Drawing.Size(23, 12);
+            this.label_rgb.TabIndex = 12;
+            this.label_rgb.Text = "RGB";
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.ForeColor = System.Drawing.Color.Red;
+            this.label_info.Location = new System.Drawing.Point(90, 102);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(77, 12);
+            this.label_info.TabIndex = 13;
+            this.label_info.Text = "[label_info]";
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 171);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.label_rgb);
+            this.Controls.Add(this.textBox_rgb);
+            this.Controls.Add(this.label_blue);
+            this.Controls.Add(this.label_green);
+            this.Controls.Add(this.label_red);
+            this.Controls.Add(this.textBox_blue);
+            this.Controls.Add(this.textBox_green);
+            this.Controls.Add(this.textBox_red);
+            this.Controls.Add(this.panel_color);
+            this.Controls.Add(this.button_color);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_main";
+            this.Text = "Color Selector";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_main_FormClosing);
+            this.Load += new System.EventHandler(this.Form_main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_color;
+        private System.Windows.Forms.Panel panel_color;
+        private System.Windows.Forms.TextBox textBox_red;
+        private System.Windows.Forms.TextBox textBox_green;
+        private System.Windows.Forms.TextBox textBox_blue;
+        private System.Windows.Forms.Label label_red;
+        private System.Windows.Forms.Label label_green;
+        private System.Windows.Forms.Label label_blue;
+        private System.Windows.Forms.TextBox textBox_rgb;
+        private System.Windows.Forms.Label label_rgb;
+        private System.Windows.Forms.Label label_info;
+    }
+}
+
---
+++ ColorSelector/Form_Main.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ColorSelector
+{
+    public partial class Form_main : Form
+    {
+        public Form_main()
+        {
+            InitializeComponent();
+        }
+        #region variable
+        public static string datapath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public static string fullpath = System.Windows.Forms.Application.ExecutablePath;
+        public static string filename = System.IO.Path.GetFileNameWithoutExtension(fullpath);
+        public static string path = datapath + "\\" + filename + ".xml";
+        bool eventlock = false;
+        int[] customcolors;
+        #endregion
+        #region Event
+        private void button_color_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.AllowFullOpen = true;
+            colorDialog.AnyColor = true;
+            colorDialog.FullOpen = true;
+            colorDialog.SolidColorOnly = false;
+            colorDialog.CustomColors = customcolors;
+            colorDialog.Color = panel_color.BackColor;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                panel_color.BackColor = colorDialog.Color;
+                textBox_red.Text = colorDialog.Color.R.ToString();
+                textBox_green.Text = colorDialog.Color.G.ToString();
+                textBox_blue.Text = colorDialog.Color.B.ToString();
+                textBox_rgb.Text = ColorToRgbString(colorDialog.Color);
+                customcolors = colorDialog.CustomColors;
+            }
+        }
+
+        private void textBox_red_TextChanged(object sender, EventArgs e)
+        {
+            ColorChanged(textBox_red);
+        }
+
+        private void textBox_green_TextChanged(object sender, EventArgs e)
+        {
+            ColorChanged(textBox_green);
+        }
+
+        private void textBox_blue_TextChanged(object sender, EventArgs e)
+        {
+            ColorChanged(textBox_blue);
+        }
+
+        private void textBox_rgb_TextChanged(object sender, EventArgs e)
+        {
+            if (!eventlock)
+            {
+                try
+                {
+                    eventlock = true;
+                    label_info.Text = "";
+                    Color color = (Color)new ColorConverter().ConvertFromString(textBox_rgb.Text);
+                    panel_color.BackColor = color;
+                    textBox_red.Text = color.R.ToString();
+                    textBox_green.Text = color.G.ToString();
+                    textBox_blue.Text = color.B.ToString();
+                }
+                catch
+                {
+                    label_info.Text = "Wrong value!";
+                }
+                finally
+                {
+                    eventlock = false;
+                }
+            }
+        }
+
+        private void Form_main_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!eventlock)
+                {
+                    eventlock = true;
+                    textBox_red.Text = "255";
+                    textBox_green.Text = "255";
+                    textBox_blue.Text = "255";
+                    panel_color.BackColor = Color.White;
+                    label_info.Text = "";
+                    XmlDocument doc = new XmlDocument();
+                    if (!File.Exists(path))
+                    {
+                        XmlElement root = doc.CreateElement("ColorSelector");
+                        XmlElement CustomColors = doc.CreateElement("CustomColors");
+                        XmlElement MainColor = doc.CreateElement("MainColor");
+                        XmlElement MainForm = doc.CreateElement("MainForm");
+                        MainForm.SetAttribute("Top", this.Top.ToString());
+                        MainForm.SetAttribute("Left", this.Left.ToString());
+                        for (int i = 0; i < 16; i++)
+                        {
+                            XmlElement color = doc.CreateElement("Color");
+                            color.InnerText = "16777215";
+                            CustomColors.AppendChild(color);
+                        }
+                        root.AppendChild(CustomColors);
+                        MainColor.InnerText = panel_color.BackColor.ToArgb().ToString();
+                        root.AppendChild(MainColor);
+                        root.AppendChild(MainForm);
+                        doc.AppendChild(root);
+                        WriteSetting(path, doc);
+                        //doc.Save(path);
+                    }
+                    else
+                    {
+                        doc = ReadSetting(path);
+                        //doc.Load(path);
+                        XmlElement MainForm = doc.DocumentElement["MainForm"];
+                        this.Top = Convert.ToInt32(MainForm.GetAttribute("Top"));
+                        this.Left= Convert.ToInt32(MainForm.GetAttribute("Left"));
+                        XmlNodeList CustomColorsList = doc.DocumentElement["CustomColors"].ChildNodes;
+                        if (customcolors == null)
+                            customcolors = new int[CustomColorsList.Count];
+                        for (int i = 0; i < CustomColorsList.Count; i++)
+                        {
+                            customcolors[i] = Convert.ToInt32(CustomColorsList[i].InnerText);
+                        }
+                        XmlNode MainColor = doc.DocumentElement["MainColor"];
+                        Color color = Color.FromArgb(Convert.ToInt32(MainColor.InnerText));
+                        textBox_red.Text = color.R.ToString();
+                        textBox_green.Text = color.G.ToString();
+                        textBox_blue.Text = color.B.ToString();
+                        textBox_rgb.Text = ColorToRgbString(color);
+                        panel_color.BackColor = color;
+                    }
+                }
+            }
+            catch
+            {
+                label_info.Text = "Wrong value!";
+            }
+            finally
+            {
+                eventlock = false;
+            }
+        }
+
+        private void Form_main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc = ReadSetting(path);
+                //doc.Load(path);
+                if (customcolors != null)
+                {
+                    XmlNodeList CustomColorsList = doc.DocumentElement["CustomColors"].ChildNodes;
+                    for (int i = 0; i < CustomColorsList.Count; i++)
+                    {
+                        CustomColorsList[i].InnerText = customcolors[i].ToString();
+                    }
+                }
+                XmlNode MainColor = doc.DocumentElement["MainColor"];
+                XmlElement MainForm = doc.DocumentElement["MainForm"];
+                MainForm.SetAttribute("Top", this.Top.ToString());
+                MainForm.SetAttribute("Left", this.Left.ToString());
+                MainColor.InnerText = panel_color.BackColor.ToArgb().ToString();
+                WriteSetting(path, doc);
+                //doc.Save(path);
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                Environment.Exit(0);
+            }
+        }
+        #endregion
+        #region Functions
+        private string ColorToRgbString(Color color)
+        {
+            string red = Convert.ToString(color.R, 16);
+            string green = Convert.ToString(color.G, 16);
+            string blue = Convert.ToString(color.B, 16);
+            if (red.Length < 2)
+            {
+                red = "0" + red;
+            }
+            if (green.Length < 2)
+            {
+                green = "0" + green;
+            }
+            if (blue.Length < 2)
+            {
+                blue = "0" + blue;
+            }
+            string RgbString = "#" + red + green + blue;
+            return RgbString;
+        }
+        private void ColorChanged(TextBox textBox)
+        {
+            if (!eventlock)
+            {
+                try
+                {
+                    eventlock = true;
+                    label_info.Text = "";
+                    bool isred = int.TryParse(textBox_red.Text, out int red);
+                    bool isgreen = int.TryParse(textBox_green.Text, out int green);
+                    bool isblue = int.TryParse(textBox_blue.Text, out int blue);
+                    if (!isred || !isgreen || !isblue)
+                    {
+                        label_info.Text = "Wrong value!";
+                    }
+                    Color color = Color.FromArgb(red, green, blue);
+                    panel_color.BackColor = color;
+                    if (textBox != textBox_red)
+                        textBox_red.Text = color.R.ToString();
+                    if (textBox != textBox_green)
+                        textBox_green.Text = color.G.ToString();
+                    if (textBox != textBox_blue)
+                        textBox_blue.Text = color.B.ToString();
+                    textBox_rgb.Text = ColorToRgbString(color);
+                }
+                catch
+                {
+                    label_info.Text = "Wrong value!";
+                }
+                finally
+                {
+                    eventlock = false;
+                }
+            }
+        }
+        #endregion
+        #region XmlOperation
+        private XmlDocument ReadSetting(string FilePath)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlReaderSettings readerSettings = new XmlReaderSettings();
+            readerSettings.IgnoreComments = true;
+            XmlReader xmlReader = XmlReader.Create(FilePath, readerSettings);
+            xmlDocument.Load(xmlReader);
+            xmlReader.Close();
+            return xmlDocument;
+        }
+        private void WriteSetting(string FilePath, XmlDocument xmlDocument)
+        {
+            XmlWriterSettings writerSettings = new XmlWriterSettings();
+            writerSettings.Encoding = Encoding.UTF8;
+            writerSettings.Indent = true;
+            XmlWriter xmlWriter = XmlWriter.Create(FilePath, writerSettings);
+            xmlDocument.Save(xmlWriter);
+            xmlWriter.Close();
+        }
+        #endregion
+    }
+} 
